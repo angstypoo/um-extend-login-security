afb46f914a1c7866e7468ddf3e5844beee02d062
angstypoo <bryce@umethod.net>
2018-09-18 18:54:01 -0700

    Update Tabs

--- um-extend-login-security.php
+++ um-extend-login-security.php
@@ -17,7 +17,6 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       um-extend-login-security
- * Domain Path:       /languages
  */
 
 // If this file is called directly, abort.
